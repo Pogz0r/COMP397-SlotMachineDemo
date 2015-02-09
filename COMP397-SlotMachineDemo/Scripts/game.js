8a58ae444876bdc59cbbb4bb7a3b68969869cc47
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-02-09 15:18:09 -0500

    Added Event Handlers for spinButton

--- COMP397-SlotMachineDemo/Scripts/game.js
+++ COMP397-SlotMachineDemo/Scripts/game.js
@@ -29,25 +29,62 @@ function gameLoop() {
     stage.update();
 }
 
-function buttonClicked() {
+function spinButtonClicked() {
+    console.log("Spin Button Clicked");
 }
 
-function buttonOut() {
+function spinButtonOut() {
+    spinButton.alpha = 1; // 100% Alpha
 }
 
-function buttonOver() {
+function spinButtonOver() {
+    spinButton.alpha = 0.7;
+}
+
+function createUI() {
+    background = new createjs.Bitmap("assets/images/background.png");
+    game.addChild(background); // Add the background to the game container
+
+    // Spin Button
+    spinButton = new createjs.Bitmap("assets/images/spinButton.png");
+    game.addChild(spinButton);
+    spinButton.x = 410;
+    spinButton.y = 545;
+
+    // Spin Button Event Listeners
+    spinButton.addEventListener("click", spinButtonClicked);
+    spinButton.addEventListener("mouseover", spinButtonOver);
+    spinButton.addEventListener("mouseout", spinButtonOut);
+
+    // Bet Max Button
+    betMaxButton = new createjs.Bitmap("assets/images/betMaxButton.png");
+    game.addChild(betMaxButton);
+    betMaxButton.x = 325;
+    betMaxButton.y = 560;
+
+    // Bet One Button
+    betOneButton = new createjs.Bitmap("assets/images/betOneButton.png");
+    game.addChild(betOneButton);
+    betOneButton.x = 235;
+    betOneButton.y = 560;
+
+    // Reset Button
+    resetButton = new createjs.Bitmap("assets/images/resetButton.png");
+    game.addChild(resetButton);
+    resetButton.x = 150;
+    resetButton.y = 560;
+
+    // Power Button
+    powerButton = new createjs.Bitmap("assets/images/powerButton.png");
+    game.addChild(powerButton);
+    powerButton.x = 55;
+    powerButton.y = 560;
 }
 
 function main() {
     game = new createjs.Container(); // Instantiates the Game Container
 
-    background = new createjs.Bitmap("assets/images/background.png");
-    game.addChild(background); // Add the background to the game container
-
-    spinButton = new createjs.Bitmap("assets/images/spinButton.png");
-    game.addChild(spinButton);
-    spinButton.x = 410;
-    spinButton.y = 545;
+    createUI();
 
     stage.addChild(game); // Adds the Game Container to the Stage
 }
