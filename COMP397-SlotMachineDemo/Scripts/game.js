87069e90bc5d1e42011ca41b2d066f7b5db1db12
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-02-09 14:55:59 -0500

    added image assets to project

--- COMP397-SlotMachineDemo/Scripts/game.js
+++ COMP397-SlotMachineDemo/Scripts/game.js
@@ -1,27 +1,19 @@
 ﻿// CreateJS Boilerplate for COMP397
-
-
 // VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-var canvas; // Reference to the HTML 5 Canvas element
-var stage: createjs.Stage; // Reference to the Stage
-
+var canvas;
+var stage;
 
 // GAME OBJECTS
-var game: createjs.Container; // Main Game Container Object
-var background: createjs.Bitmap;
-var spinButton: createjs.Bitmap;
-var betMaxButton: createjs.Bitmap;
-var betOneButton: createjs.Bitmap;
-var resetButton: createjs.Bitmap;
-var powerButton: createjs.Bitmap;
-
+var game;
+var background;
+var spinButton;
+var betMaxButton;
+var betOneButton;
+var resetButton;
+var powerButton;
 
 // FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 function init() {
-
-
-
     canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas); // Parent Object
     stage.enableMouseOver(20); // Turn on Mouse Over events
@@ -32,24 +24,19 @@ function init() {
     main();
 }
 
-
 // GAMELOOP
 function gameLoop() {
     stage.update();
 }
 
 function buttonClicked() {
-
 }
 
 function buttonOut() {
-
 }
 
 function buttonOver() {
-
 }
-
 
 function main() {
     game = new createjs.Container(); // Instantiates the Game Container
@@ -62,11 +49,6 @@ function main() {
     spinButton.x = 410;
     spinButton.y = 545;
 
-
     stage.addChild(game); // Adds the Game Container to the Stage
-    
-
 }
-
-
-
+//# sourceMappingURL=game.js.map
